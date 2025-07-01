b40743db31fb5142c96b21b85dbb94ae66678afd
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-01 17:01:45 +0100

    Finish exercise 2

--- 2.Print Every N-th Element from an Array.js
+++ 2.Print Every N-th Element from an Array.js
@@ -1,5 +1,12 @@
 function PrintEveryElementFromAnArray(arr, rotationNum) {
+        let arrOfNthElements = [];
+        for (let index = 0; index < arr.length; index+=rotationNum) {
+            const number = arr[index];
+            arrOfNthElements.push(number)
+        }
 
+        return arrOfNthElements;
+        
 }
 
 PrintEveryElementFromAnArray(['5','20', '31', '4', '20'], 2)
