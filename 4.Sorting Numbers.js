002d016bbfe06c1588f0cd9bcccc58d765de397d
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-01 17:36:21 +0100

    Finish exercise 4

--- 4.Sorting Numbers.js
+++ 4.Sorting Numbers.js
@@ -1,5 +1,18 @@
 function sortNumbers(arr) {
-   
+    let result = [];
+    arr.sort((a, b) => a - b); 
+
+    while (arr.length > 0) {
+        if (arr.length > 0) {
+            result.push(arr.shift()); 
+        }
+        if (arr.length > 0) {
+            result.push(arr.pop());   
+        }
+    }
+
+    console.log(result);
+    
 }
 
 sortNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56])
