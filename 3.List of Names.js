eeae0e39cbba9d26b7bfaff4af850196dcbc0570
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-01 17:13:20 +0100

    Finish exercise 3

--- 3.List of Names.js
+++ 3.List of Names.js
@@ -1,5 +1,7 @@
 function ListOfNames(arr) {
-    
+    arr.sort().forEach((name, index) => {
+        console.log(`${index + 1}.${name}`);
+    });
 }
 
 ListOfNames(["John", "Bob", "Christina", "Ema"])
