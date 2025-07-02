0b5a1c68af6753ebf06651fb47d4cfc8a4034304
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-02 14:33:50 +0100

    Finish exercise 5

--- 5.Reveal Words.js
+++ 5.Reveal Words.js
@@ -1,5 +1,18 @@
 function revealWords(keyWords, sentence) {
-       
+        keyWords = keyWords.split(", ");
+        sentenceArr = sentence.split(' ');
+        for (const keyWord of keyWords) {
+            for (const word of sentenceArr) {
+             if (word.includes("*")) {
+               sentence = sentence.replace(word, keyWord);
+               let indexOfWord = sentenceArr.indexOf(word); 
+                sentenceArr.splice(indexOfWord, 1)
+               break;
+            }    
+        }   
+    }
+   return sentence
+   
 }
 
 
