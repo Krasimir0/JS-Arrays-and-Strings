8fe9000e9c8620a5a93130d634f9f2394e263310
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-02 16:56:00 +0100

    Finish exercise 6

--- 6.Modern Times of #(HashTag).js
+++ 6.Modern Times of #(HashTag).js
@@ -1,5 +1,17 @@
 function GetWordsWithHashTag(sentence) {
-    
+    const words = sentence.split(" ");
+    const result = [];
+
+    for (let word of words) {
+        if (word.startsWith("#") && word.length > 1) {
+            const tag = word.slice(1);
+            if (/^[A-Za-z]+$/.test(tag)) {
+                result.push(`#${tag}`);
+            }
+        }
+    }
+
+    return result;
 }
 
 GetWordsWithHashTag('Nowadays everyone uses # to tag a #special word in #socialMedia')
