f7033fef807b68a32eb0e828e0158c2836c228a5
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2025-07-02 14:40:03 +0100

    Add exercise 6

--- 5.Reveal Words.js
+++ 5.Reveal Words.js
@@ -10,7 +10,7 @@ function revealWords(keyWords, sentence) {
                break;
             }    
         }   
-    }
+    }   
    return sentence
    
 }
---
+++ 6.Modern Times of #(HashTag).js
@@ -0,0 +1,5 @@
+function GetWordsWithHashTag(sentence) {
+    
+}
+
+GetWordsWithHashTag('Nowadays everyone uses # to tag a #special word in #socialMedia')
